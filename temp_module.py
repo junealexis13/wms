0bcb1279537ddd06eb0bf4eec2df0ef196fd72bf
junealexis13 <junealexis.santos13@gmail.com>
2025-10-21 17:18:33 +0800

    updated tempModule

--- temp_module.py
+++ temp_module.py
@@ -1,25 +1,43 @@
 import subprocess
+import time
 
 class TemperatureModule:
-    '''Reads DS18B20 Temperature Sensor in °C'''
+    """Reads DS18B20 Temperature Sensor in °C"""
     def __init__(self):
         pass
 
     def fetch_temp(self):
         try:
+            # Use shell=True with a string command so wildcard expands
             result = subprocess.run(
-                ["cat", "/sys/bus/w1/devices/28-*/temperature"],
+                "cat /sys/bus/w1/devices/28-*/temperature",
                 capture_output=True,
                 text=True,
                 shell=True,
                 check=True
             )
-            return float(result.stdout.strip())/1000.0
-        
+
+            # Check if there's output
+            if result.stdout.strip():
+                temp_c = float(result.stdout.strip()) / 1000.0
+                print(f"Temperature: {temp_c:.2f} °C")
+                return temp_c
+            else:
+                print("No temperature data received.")
+                return None
+
         except subprocess.CalledProcessError as e:
             print("Error reading sensor:", e)
             return None
-        
+
         except ValueError:
             print("Invalid temperature format")
             return None
+
+
+
+if __name__ == "__main__":
+    mod = TemperatureModule()
+    while True:
+        time.sleep(1)
+        mod.fetch_temp()
